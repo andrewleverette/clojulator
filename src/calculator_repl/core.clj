a6406752e6b0c0434679311697160baee4d2b663
Andrew Leverette <andrew.leverette@antage.net>
2024-10-06 21:07:28 -0500

    Add evaluator and wire everything up. Still needs some work

--- src/calculator_repl/core.clj
+++ src/calculator_repl/core.clj
@@ -1,8 +1,10 @@
 (ns calculator-repl.core
   (:gen-class)
   (:require
-   [clojure.string :as str]
    [calculator-repl.printer :as printer]
+   [calculator-repl.evaluator :as eva]
+   [calculator-repl.reader.scanner :as lex]
+   [calculator-repl.reader.parser :as par]
    [calculator-repl.reader.core :as rdr]))
 
 (defn display-welcome-message
@@ -15,94 +17,15 @@
   (printer/display-line "Type 'quit' to exit")
   (printer/display-line "================================================"))
 
-(defn is-number?
-  [s]
-  (->> s
-       str/trim
-       parse-long))
-
-(defn is-operator?
-  [s]
-  (->> s
-       str/trim
-       #{"+" "-" "*" "/"}))
-
-(defn str->int
-  [s]
-  (-> s str/trim parse-long))
-
-(defn str->operator
-  [s]
-  (->
-   s
-   str/trim
-   (case
-    "+" :Plus
-    "-" :Minus
-    "*" :Times
-    "/" :Divide)))
-
-(defn tokens->str
-  [tokens]
-  (->> tokens
-       (map :term)
-       (str/join "")))
-
-(defn read-expression
-  []
-  (printer/display "(in)=> " :with-flush? true)
-  (read-line))
-
 (defn should-quit?
   [s]
   (= s :quit))
 
-(defn tokenize [input]
-  (loop [terms (-> input (str/split #"(?<=[-+*/])|(?=[-+*/])") seq)
-         tokens []]
-    (if (seq terms)
-      (let [term (first terms)]
-        (cond
-          (is-number? term) (recur (rest terms) (conj tokens {:term term
-                                                              :value (str->int term)}))
-          (is-operator? term) (recur (rest terms) (conj tokens {:term term
-                                                                :value (str->operator term)}))
-          :else (throw (Exception. (str "Invalid token: " term)))))
-      tokens)))
-
-(defn parse
-  [tokens]
-  (if (= (count tokens) 3)
-    (let [[x op y] (map :value tokens)]
-      (if (and (= op :Divide) (zero? y))
-        (throw (Exception. (str "Divide by zero error: " (tokens->str tokens))))
-        [op x y]))
-    (throw (Exception. (apply str "Invalid expression: " (tokens->str tokens))))))
-
-(defn expression->str
-  [exp]
-  (let [op (case (first exp)
-             :Plus "+"
-             :Minus "-"
-             :Times "*"
-             :Divide "/")
-        operands (rest exp)]
-    (str (first operands) " " op " " (second operands))))
-
-(defn evaluate
-  [exp]
-  (let [[op x y] exp]
-    (case op
-      :Plus (+ x y)
-      :Minus (- x y)
-      :Times (* x y)
-      :Divide (/ x y))))
-
 (defn try-evaluate
   [exp]
   (try
-    (let [parsed (-> exp tokenize parse)
-          result (evaluate parsed)]
+    (let [parsed (-> exp lex/tokenize par/parse)
+          result (eva/evaluate parsed)]
       {:ok result})
     (catch Exception e
       {:error (.getMessage e)})))
@@ -110,7 +33,7 @@
 (defn display-result
   [result]
   (if (:ok result)
-    (printer/display-line (str "(out)=> " (expression->str (:ok result))))
+    (printer/display-line (str "(out)=> " (:ok result)))
     (printer/display-line (str "(error)=> " (:error result)))))
 
 (defn -main
@@ -123,4 +46,3 @@
         (do
           (-> input try-evaluate display-result)
           (recur))))))
-
