9c99e6b3e4a935374b769db17005391d569d6d8f
Andrew Leverette <andrew.leverette@antage.net>
2024-10-19 12:03:28 -0500

    Add base layout for calculator

--- src/clojulator/web/core.cljs
+++ src/clojulator/web/core.cljs
@@ -13,7 +13,8 @@
  (fn [_ _]
    {:history (atom [])
     :last-expression ""
-    :last-result ""}))
+    :last-result ""
+    :display ""}))
 
 (rf/reg-event-db
  :calculate
@@ -22,6 +23,11 @@
      (-> db
          (assoc :last-expression expression)
          (assoc :last-result result)))))
+
+(rf/reg-event-db
+ :update-display
+ (fn [db [_ c]]
+   (update db :display str c)))
 
 ;; ----------------------------------
 ;; Subscriptions
@@ -34,6 +40,10 @@
  :last-result
  (fn [db _] (:lastResult db)))
 
+(rf/reg-sub
+ :display
+ (fn [db _] (:display db)))
+
 ;; ----------------------------------
 ;; Views
 
@@ -45,17 +55,60 @@
    [:h4 "A calculator written in Clojure"]])
 
 (defn display
-  [output])
+  []
+  (let [display (rf/subscribe [:display])]
+    [:div
+     {:class "w-full h-16 sm:h-28 bg-white border-2 border-blue-400 rounded-lg mt-5 md:mt-0 flex flex-col justify-evenly items-end pr-2 border-blue-500"}
+     [:div
+      {:class "place-self-center py-1"}
+      [:div
+       {:id "previous-expression"}]
+      [:div
+       {:id "current-display"
+        :class "text-2xl md:text-3xl font-semibold"} @display]]]))
 
-(defn calculator-view
-  [])
+(defn symbol-keys
+  []
+  (let [operators ["-" "+" "/" "*" "%" "^" "(" ")"]]
+    (fn []
+      [:div
+       {:class "basic-operations col-span-3 grid grid-cols-2 [&>*]:py-5 [&>*]:px-2.5 [&>*]:sm:px-6 gap-2 [&>*:hover]:bg-blue-900 [&>*]:rounded [&>*:hover]:text-white [&>*:active]:scale-90"}
+       [:button {:key "clear"
+                 :class "col-span-2 bg-slate-300"} "CLR"]
+       (map #(vector :button {:key % :class "bg-slate-300"} %) operators)])))
+
+(defn numeric-keys
+  []
+  (let [numbers [7 8 9 4 5 6 1 2 3 0 "."]]
+    (fn []
+      [:div
+       {:class "number mr-2 col-span-9 grid grid-cols-3 gap-2 [&>*:hover]:bg-blue-900 [&>*]:rounded [&>*:hover]:text-white [&>*:active]:scale-90"}
+       (map #(vector :button {:key % :class "bg-slate-100"} %) numbers)
+       [:button {:key "enter"
+                 :class "bg-[#3651c4] text-white"} "="]])))
+
+(defn keypad
+  []
+  [:div
+   {:class "bg-white text-xl w-full p-1 rounded-lg grid grid-cols-12"}
+   [numeric-keys]
+   [symbol-keys]])
+
+(defn calculator
+  []
+  [:div
+   {:class "flex flex-col items-center gap-3 font-bold"}
+   [display]
+   [keypad]])
 
 (defn index
   []
   [:div
-   {:class "flex flex-col items-center min-h-screen"}
-   [header]
-   [calculator-view]])
+   {:class "bg-slate-400 flex flex-col items-center min-h-screen"}
+   [:main
+    {:class "w-full max-w-lg select-none px-4 md:px-0"}
+    [header]
+    [calculator]]])
 
 ;; ----------------------------------
 ;; Entry Point
