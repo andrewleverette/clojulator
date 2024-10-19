1c9fbfe472262a491df0d35ad94337e3897d8aef
Andrew Leverette <andrew.leverette@antage.net>
2024-10-19 08:34:13 -0500

    Remove generated JavaScript files

--- resources/public/js/cljs-runtime/shadow.module.client.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('client');
-
-try { clojulator.web.core.main(); } catch (e) { console.error("An error occurred when calling (clojulator.web.core/main)"); console.error(e); }
