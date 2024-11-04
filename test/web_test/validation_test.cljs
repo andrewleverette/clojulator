b2fbcb30a9562de50a04ae83a0f997755353d7c3
Andrew Leverette <andrew.leverette@antage.net>
2024-11-04 16:19:27 -0600

    Add tests for validation state changes

---
+++ test/web_test/validation_test.cljs
@@ -0,0 +1,100 @@
+(ns web-test.validation-test
+  (:require [cljs.test :refer-macros [deftest is testing]]
+            [clojulator.web.validation :refer [validate-user-input]]))
+
+(deftest validation-state-change-tests
+  (testing "Initial state transitions"
+    (is (= :validation/after-open-paren
+           (validate-user-input {:state/validation-state :validation/start
+                                 :state/display ""} "(")))
+    (is (= :validation/after-unary-operator
+           (validate-user-input {:state/validation-state :validation/start
+                                 :state/display ""} "-")))
+    (is (= :validation/after-number
+           (validate-user-input {:state/validation-state :validation/start
+                                 :state/display ""} 5)))
+    (is (= :validation/invalid
+           (validate-user-input {:state/validation-state :validation/start
+                                 :state/display ""} ".")))
+    (is (= :validation/invalid
+           (validate-user-input {:state/validation-state :validation/start
+                                 :state/display ""} "+")))
+    (is (= :validation/invalid
+           (validate-user-input {:state/validation-state :validation/start
+                                 :state/display ""} "invalid"))))
+
+  (testing "Transition from after-open-paren state"
+    (is (= :validation/after-number
+           (validate-user-input {:state/validation-state :validation/after-open-paren
+                                 :state/display "("} 5)))
+    (is (= :validation/after-unary-operator
+           (validate-user-input {:state/validation-state :validation/after-open-paren
+                                 :state/display "("} "-")))
+    (is (= :validation/after-open-paren
+           (validate-user-input {:state/validation-state :validation/after-open-paren
+                                 :state/display "("} "(")))
+    (is (= :validation/invalid
+           (validate-user-input {:state/validation-state :validation/after-open-paren
+                                 :state/display "("} "invalid"))))
+
+  (testing "Transition from after-close-paren state"
+    (is (true? (every? #(= :validation/after-operator %)
+                       (map #(validate-user-input {:state/validation-state :validation/after-number
+                                                   :state/display ")"} %)
+                            ["+" "-" "*" "/" "^" "%"]))))
+    (is (= :validation/invalid
+           (validate-user-input {:state/validation-state :validation/after-number
+                                 :state/display ")"} "invalid"))))
+
+  (testing "Transition from after-unary-operator state"
+    (is (= :validation/after-number
+           (validate-user-input {:state/validation-state :validation/after-unary-operator
+                                 :state/display "-"} 5)))
+    (is (= :validation/after-open-paren
+           (validate-user-input {:state/validation-state :validation/after-unary-operator
+                                 :state/display "-"} "(")))
+    (is (= :validation/invalid
+           (validate-user-input {:state/validation-state :validation/after-unary-operator
+                                 :state/display "-"} "invalid"))))
+
+  (testing "Transition from after-number state"
+    (is (= :validation/after-number
+           (validate-user-input {:state/validation-state :validation/after-number
+                                 :state/display "5"} 5)))
+    (is (true? (every? #(= :validation/after-operator %)
+                       (map #(validate-user-input {:state/validation-state :validation/after-number
+                                                   :state/display "5"} %)
+                            ["+" "-" "*" "/" "^" "%"]))))
+    (is (= :validation/after-close-paren
+           (validate-user-input {:state/validation-state :validation/after-number
+                                 :state/display "5"} ")")))
+    (is (= :validation/after-decimal
+           (validate-user-input {:state/validation-state :validation/after-number
+                                 :state/display "5"} ".")))
+    (is (= :validation/invalid
+           (validate-user-input {:state/validation-state :validation/after-number
+                                 :state/display "5.0"} ".")))
+    (is (= :validation/invalid
+           (validate-user-input {:state/validation-state :validation/after-number
+                                 :state/display "5"} "invalid"))))
+
+  (testing "Transition from after-operator state"
+    (is (= :validation/after-open-paren
+           (validate-user-input {:state/validation-state :validation/after-operator
+                                 :state/display "+"} "(")))
+    (is (= :validation/after-unary-operator
+           (validate-user-input {:state/validation-state :validation/after-operator
+                                 :state/display "+"} "-")))
+    (is (= :validation/after-number
+           (validate-user-input {:state/validation-state :validation/after-operator
+                                 :state/display "+"} 5)))
+    (is (= :validation/invalid
+           (validate-user-input {:state/validation-state :validation/after-operator
+                                 :state/display "+"} "invalid"))))
+  (testing "Transition from after-decimal state"
+    (is (= :validation/after-number
+           (validate-user-input {:state/validation-state :validation/after-decimal
+                                 :state/display "5."} 5)))
+    (is (= :validation/invalid
+           (validate-user-input {:state/validation-state :validation/after-decimal
+                                 :state/display "5."} "invalid")))))
